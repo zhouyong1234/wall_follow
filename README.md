0760a089aaacc2a7e1498a0a7f172fc34161e321
zhouyong <yong.zhou@touchair.cn>
2022-04-29 11:09:25 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# wall_follow
-沿墙搜索算法
+# wall_follower
+
+## 沿墙搜索算法
+### 接收融合后的2D激光数据,将激光数据划分为不同区域,通过不同区域测到的激光与障碍物的距离给出不同的运动指令,使机器人始终靠着右侧的墙壁运动
+#### wall_follower.launch为启动节点
+#### Subscribed Topics
+ - topic: /combined_scan
+ - type: sensor_msgs/LaserScan
+#### Published Topics
+ - topic: /cmd_vel
+ - type: geometry_msgs/Twist
+
---
+++ src/follow_wall.cpp
@@ -0,0 +1,301 @@
+/*
+Basic Follow Wall Algorithm
+Author: Roberto Zegers
+Date: 2019-March-27
+*/
+
+// ROS Libraries
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h> // Laser Data
+#include <geometry_msgs/Twist.h> // Motor Commands
+
+// ROS Subscriber for laser data and messages type (LaserScan)
+ros::Subscriber laser_subscriber;
+sensor_msgs::LaserScan laser_msg;
+
+// ROS Publisher for motor commands and message type (Twist)
+ros::Publisher motor_command_publisher;
+geometry_msgs::Twist motor_command;
+
+// global array to keep track of the min. distance value on each zone
+float a, b;
+float z[5];
+// global variable used to set the robots's driving logic
+int state;
+
+// The laser_callback function is called each time laser scan data is received
+void laser_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg) {
+  // Read and process laser scan values
+  laser_msg = *scan_msg;
+  // declare an array of float values to keep track of the laser measurements
+  std::vector<float> laser_rays;
+  // note: array of laser range values is filled from right to left (CCW)
+  // rostopic list shows 45*16 = 720 individual laser rays published
+  laser_rays = laser_msg.ranges;
+  // the total number of laser rays the laser range finder has
+  size_t range_size = laser_rays.size();
+  ROS_INFO_ONCE("Number of laser rays: [%zu]", range_size); // for debugging
+  // variables to store closest (min) distance values on each zone
+  z[0] = laser_msg.range_max;
+  z[1] = laser_msg.range_max;
+  z[2] = laser_msg.range_max;
+  z[3] = laser_msg.range_max;
+  z[4] = laser_msg.range_max;
+
+
+  ROS_INFO("range size: [%d] ", range_size);
+
+
+  float range_max = laser_msg.range_min;
+  // cycle trough all laser range rays
+
+  for(size_t i = 0, j = 1368; i < range_size * 1 / 20, j < range_size; i++, j++)
+  {
+      if (laser_rays[i] < z[2])
+      {
+          z[2] = laser_rays[i];
+      }
+      if (laser_rays[j] < z[2])
+      {
+          z[2] = laser_rays[j];
+      }
+  }
+
+  for(size_t i = 72; i < range_size * 1 / 4; i++)
+  {
+      if(i < range_size * 3 / 20)
+      {
+          if(laser_rays[i] < z[3])
+          {
+              z[3] = laser_rays[i];
+          }
+      }
+      else if(i >= range_size * 3 / 20 && i < range_size * 1 / 4)
+      {
+          if(laser_rays[i] < z[4])
+          {
+              z[4] = laser_rays[i];
+          }
+      }
+      else
+      {
+          ROS_ERROR("Ray index not found in range size");
+      }
+  }
+
+  for(size_t i = 1080; i < range_size * 19 / 20; i++)
+  {
+      if(i < range_size * 17 / 20)
+      {
+          if(laser_rays[i] < z[0])
+          {
+              z[0] = laser_rays[i];
+          }
+      }
+      else if(i >= range_size * 17 / 20 && i < range_size * 19 / 20)
+      {
+          if(laser_rays[i] < z[1])
+          {
+              z[1] = laser_rays[i];
+          }
+      }
+      else
+      {
+          ROS_ERROR("Ray index not found in range size");
+      }
+  }
+
+  ROS_INFO("z[0]: [%f]: ", z[0]);
+  ROS_INFO("z[1]: [%f]: ", z[1]);
+  ROS_INFO("z[2]: [%f]: ", z[2]);
+  ROS_INFO("z[3]: [%f]: ", z[3]);
+  ROS_INFO("z[4]: [%f]: ", z[4]);
+}
+
+/*
+Detemine velocity commands for each state and fill in a Twist message
+*/
+
+void robot_move(geometry_msgs::Twist &motor_command) {
+  // ROS_INFO("Wall follower state: [%d]", state);
+  switch (state) {
+  case 0:
+    // Find a wall: turn CW (right) while moving ahead
+    motor_command.linear.x = 0.15;
+    motor_command.angular.z = -0.15;
+    break;
+
+  case 1:
+    // Turn left
+    motor_command.linear.x = 0.0;
+    motor_command.angular.z = 0.3;
+    break;
+
+  case 2:
+    // Follow the wall: keep moving straight ahead
+    motor_command.angular.z = 0.0;
+    motor_command.linear.x = 0.15;
+    break;
+
+  case 3:
+    // Move slow straight ahead
+    motor_command.linear.x = 0.10;
+    motor_command.angular.z = 0.0;
+    break;
+
+  case 4:
+    // Reverse turning left
+    motor_command.linear.x = -0.3;
+    motor_command.angular.z = 0.2; // pos. value equals turning C
+    break;
+  }
+}
+
+/*
+Determine the state of the environment surroundings
+and the logic used to drive the robot (using 5 zones)
+*/
+void drive_logic() {
+  // fine tune distance (mt) used to consider a region as blocked by an obstacle
+  float d = 0.5;
+
+// logic block 1:
+  if (z[0] > d && z[1] > d && z[2] > d && z[3] > d && z[4] > d) {
+    ROS_INFO("case 1: no obstacles detected");
+    state = 0; // find wall: turn CW and move ahead
+  } else if (z[0] > d && z[1] > d && z[2] < d && z[3] > d && z[4] > d) {
+    ROS_INFO("case 2: obstacle only in front zone");
+    state = 1; // turn left
+  } else if (z[0] > d && z[1] < d && z[2] > d && z[3] > d && z[4] > d) {
+    ROS_INFO("case 3: obstacle only in front-right zone");
+    state = 1; // turn left
+  } else if (z[0] > d && z[1] > d && z[2] > d && z[3] < d && z[4] > d) {
+    ROS_INFO("case 4: obstacle only in front-left zone");
+    state = 0; // find wall: turn CW and move ahead
+  } else if (z[0] > d && z[1] < d && z[2] < d && z[3] > d && z[4] > d) {
+    ROS_INFO("case 5: obstacle in front-right and front zone");
+    state = 1; // turn left
+  } else if (z[0] > d && z[1] > d && z[2] < d && z[3] < d && z[4] > d) {
+    ROS_INFO("case 6: obstacle in front and front-left zone");
+    state = 1; // turn left
+  } else if (z[0] > d && z[1] < d && z[2] < d && z[3] < d && z[4] > d) {
+    ROS_INFO("case 7: obstacle in front-right, front and front-left zone");
+    state = 1; // turn left
+  } else if (z[0] > d && z[1] < d && z[2] > d && z[3] < d && z[4] > d) {
+    ROS_INFO("case 8: obstacle in front-right and front-left zone");
+    state = 3; // move slow straight ahead
+  }
+  // logic block 2:
+  else if (z[0] < d && z[1] > d && z[2] > d && z[3] > d && z[4] > d) {
+    ROS_INFO("case 9:  obstacle only in right zone");
+    state = 2; // follow the wall: keep moving straight ahead
+  } else if (z[0] < d && z[1] > d && z[2] < d && z[3] > d && z[4] > d) {
+    ROS_INFO("case 10:  obstacle in right and front zone");
+    state = 1; // turn left
+  } else if (z[0] < d && z[1] < d && z[2] > d && z[3] > d && z[4] > d) {
+    ROS_INFO("case 11: obstacle in right and front-right zone");
+    state = 1; // turn left
+  } else if (z[0] < d && z[1] > d && z[2] > d && z[3] < d && z[4] > d) {
+    ROS_INFO("case 12: obstacle in right and front-left zone");
+    state = 3; // move slow straight ahead
+  } else if (z[0] < d && z[1] < d && z[2] < d && z[3] > d && z[4] > d) {
+    ROS_INFO("case 13: obstacle in right, front-right and front zone");
+    state = 1; // turn left
+  } else if (z[0] < d && z[1] > d && z[2] < d && z[3] < d && z[4] > d) {
+    ROS_INFO("case 14: obstacle in right, front and front-left zone");
+    state = 1; // turn left
+  } else if (z[0] < d && z[1] < d && z[2] < d && z[3] < d && z[4] > d) {
+    ROS_INFO("case 15: obst. in right, front-right, front and front-left zone");
+    state = 1; // turn left
+  } else if (z[0] < d && z[1] < d && z[2] > d && z[3] < d && z[4] > d) {
+    ROS_INFO("case 16: obstacle in right, front-right and front-left zone");
+    state = 3; // move slow straight ahead
+  }
+  // logic block 3:
+  else if (z[0] > d && z[1] > d && z[2] > d && z[3] > d && z[4] < d) {
+    ROS_INFO("case 17: obstacle only in left zone");
+    state = 0; // find wall: turn CW and move ahead
+  } else if (z[0] > d && z[1] > d && z[2] < d && z[3] > d && z[4] < d) {
+    ROS_INFO("case 18: obstacle in front and left zone");
+    state = 0; // find wall: turn CW and move ahead
+  } else if (z[0] > d && z[1] < d && z[2] > d && z[3] > d && z[4] < d) {
+    ROS_INFO("case 19: obstacle in front-right and left zone");
+    state = 3; // move slow straight ahead
+  } else if (z[0] > d && z[1] > d && z[2] > d && z[3] < d && z[4] < d) {
+    ROS_INFO("case 20: obstacle in front-left and left zone");
+    state = 0; // find wall: turn CW and move ahead
+  } else if (z[0] > d && z[1] < d && z[2] < d && z[3] > d && z[4] < d) {
+    ROS_INFO("case 21: obstacle in front-right, front and left zone");
+    state = 0; // find wall: turn CW and move ahead
+  } else if (z[0] > d && z[1] > d && z[2] < d && z[3] < d && z[4] < d) {
+    ROS_INFO("case 22: obstacle in front, front-left and left zone");
+    state = 1; // turn left
+  } else if (z[0] > d && z[1] < d && z[2] < d && z[3] < d && z[4] < d) {
+    ROS_INFO("case 23: obst. in front-right, front, front-left and left zone");
+    state = 1; // turn left
+  } else if (z[0] > d && z[1] < d && z[2] > d && z[3] < d && z[4] < d) {
+    ROS_INFO("case 24: obstacle in front-right, front-left and left zone");
+    state = 3; // move slow straight ahead 
+  }
+  // logic block 4:
+  else if (z[0] < d && z[1] > d && z[2] > d && z[3] > d && z[4] < d) {
+    ROS_INFO("case 25: obstacle in right and left zone");
+    state = 3; // move slow straight ahead
+  } else if (z[0] < d && z[1] > d && z[2] < d && z[3] > d && z[4] < d) {
+    ROS_INFO("case 26: obstacle in right, front and left zone");
+    state = 1; // turn left
+  } else if (z[0] < d && z[1] < d && z[2] > d && z[3] > d && z[4] < d) {
+    ROS_INFO("case 27: obstacle in right, front-right and left zone");
+    state = 1; // turn left
+  } else if (z[0] < d && z[1] > d && z[2] > d && z[3] < d && z[4] < d) {
+    ROS_INFO("case 28: obstacle in right, front-left and left zone");
+    state = 0; // find wall: turn CW and move ahead
+  } else if (z[0] < d && z[1] < d && z[2] < d && z[3] > d && z[4] < d) {
+    ROS_INFO("case 29: obstacle in right, front-right, front and left zone");
+    state = 1; // turn left
+  } else if (z[0] < d && z[1] > d && z[2] < d && z[3] < d && z[4] < d) {
+    ROS_INFO("case 30: obstacle in right, front, front-left, and left zone");
+    state = 0; // find wall: turn CW and move ahead
+  } else if (z[0] < d && z[1] < d && z[2] < d && z[3] < d && z[4] < d) {
+    ROS_INFO("case 31: obst. in right, front-right, front, front-left and left zone");
+    state = 4; // reverse turning left
+  } else if (z[0] < d && z[1] < d && z[2] > d && z[3] < d && z[4] < d) {
+    ROS_INFO("case 32: obst. in right, front-right, front-left and left zone");
+    state = 3; // move slow straight ahead
+  } else {
+    ROS_INFO("Unknown case");
+  }
+}
+
+int main(int argc, char **argv) {
+  // Initialize a ROS node, change name if required
+  ros::init(argc, argv, "follow_wall_commands");
+
+  // Create a ROS NodeHandle object
+  ros::NodeHandle n;
+
+  // Inform ROS master that we will be publishing a message of type
+  // geometry_msgs::Twist on the robot actuation topic with a publishing queue
+  // size of 100
+  motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
+
+  // Subscribe to the /scan topic and call the laser_callback function
+  laser_subscriber = n.subscribe("/combined_scan", 1000, laser_callback);
+//   ros::spin();
+  ROS_INFO_ONCE("Hello World! Follow Wall Customized Version!");
+  // Loop, the laser_callback function is called when new laser messages arrives
+//   ros::Rate loop_rate(1);
+  while (ros::ok()) {
+    ros::spinOnce(); // call all the callbacks waiting to be called
+    // take action: determine the state of the environment surroundings
+    // and set the logic used to drive the robot
+    drive_logic();
+    // Fill in motor command message and publish it
+    robot_move(motor_command);
+    // Publish motor commands to the robot and wait 10ms
+    motor_command_publisher.publish(motor_command);
+    usleep(10);
+    // loop_rate.sleep();
+  }
+  return 0;
+}
